8e1a5442bf20ccb913065d7f708ee833b562a569
Arya <190189398+ryachavan@users.noreply.github.com>
2025-09-27 04:15:51 +0530

    AuthProvider init

---
+++ src/context/AuthProvider.jsx
@@ -0,0 +1,11 @@
+import { AuthProvider } from './context/AuthContext';
+
+function App() {
+  return (
+    <AuthProvider>
+      {/* Your app routes/components here */}
+    </AuthProvider>
+  );
+}
+
+export default App;
